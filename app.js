50751978ef40a5cf97bfeb5907d6f368fe9d989b
Theo02-12 <mrcouvelard@gmail.com>
2023-03-18 18:14:35 +0100

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById('menu').style.height = "20vh";
+    document.getElementById('open').style.opacity = "0";
+}
+function closeMenu(){
+    document.getElementById('menu').style.height = "0";
+    document.getElementById('open').style.opacity = "1";
+}
